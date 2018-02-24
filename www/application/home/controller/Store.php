aa2fcf3a95c79cfe3415f5b7b59025b96f213586
wangke <568768582@qq.com>
2018-02-24 16:34:02 +0800

    商店

--- www/application/home/controller/Store.php
+++ www/application/home/controller/Store.php
@@ -9,13 +9,45 @@
 namespace app\home\controller;
 
 use think\Controller;
+use think\Db;
 
 class Store extends Controller
 {
 
     public function index(){
+        $id = input('param.id');
 
+        //分页
+        $limit = 10;
+        $page = empty(input('param.page'))?1:input('param.page');
+        $offset = ( $page - 1 ) * $limit;
+
+        $where = [
+            'coupons_id'   => $id,
+            'is_use'       => 1,
+            'is_validate'  => 0
+        ];
+
+        $list = Db::name('coupons_log')->where( $where )->order('add_time desc') ->limit($offset,$limit)->select();
+        $count = Db::name('coupons_log')->where($where)->count();
+        $this->assign([
+            'list'=>$list,
+            'count'=>$count
+        ]);
         return $this->fetch();
+    }
+
+    /**
+     * 验证
+     */
+    public function validate_code(){
+        $id = input('param.id');
+        $re = Db::name('coupons_log')->where('id',$id)->update(array('is_validate'=>1));
+        if( $re ){
+            return msg(1,'','验证成功！');
+        }else{
+            return msg(-1,'','验证失败！');
+        }
     }
 
 }
